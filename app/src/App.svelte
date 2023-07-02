5e73a7bc1205219d6c6f11ba49be81f1af23ae17
riley <nrileyfletcher@gmail.com>
2023-07-02 15:17:15 -0400

    folder rn

---
+++ app/src/App.svelte
@@ -0,0 +1,39 @@
+<script>
+	import Button from "./Button.svelte";
+	import Component from "./Component.svelte";
+  	import DynamicList from "./DynamicList.svelte";
+
+
+	let name = 'Riley';
+	let banner = 'This is a <strong>BIG</strong> banner';
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<h1>{@html banner}</h1>
+	<Component></Component>
+	<Button></Button>
+	<DynamicList></DynamicList>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
