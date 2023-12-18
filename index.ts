67dd00cef6c64f6df35628512e327f8d0449ad84
riley <nrileyfletcher@gmail.com>
2023-12-18 00:47:52 -0500

    vscode commit test

--- index.ts
+++ index.ts
@@ -5,4 +5,17 @@ const func = () => {
     return 5;
 }
 
-console.log(func());
+function reverseString(s: string) {
+    if(s.length == 0) return s;
+    let chars: string[] = [...s];
+    for(let i: number = 0; i < chars.length /2 ; i++) {
+        let temp: string = chars[i];
+        chars[i] = chars[chars.length - i - 1];
+        chars[chars.length - i - 1] = temp;
+        console.log(chars);
+
+    }
+    console.log(chars);
+}
+
+reverseString("abcdefg");
