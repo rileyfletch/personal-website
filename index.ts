4de8c5963e280f1bbedecd17bc1238dc68ad4258
riley <nrileyfletcher@gmail.com>
2023-12-19 23:40:44 -0500

    test

--- index.ts
+++ index.ts
@@ -6,8 +6,10 @@ const func = () => {
 }
 
 function reverseString(s: string) {
+
     if(s.length == 0) return s;
     let chars: string[] = [...s];
+    
     for(let i: number = 0; i < chars.length /2 ; i++) {
         let temp: string = chars[i];
         chars[i] = chars[chars.length - i - 1];
