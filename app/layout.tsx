ee47b52c37cc5f0660be0d730886c7f988940b52
riley <nrileyfletcher@gmail.com>
2024-01-13 23:35:16 -0500

    header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
+import Header from '@/components/header'
 import './globals.css'
 import { Inter } from 'next/font/google'
-
+ 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -18,10 +19,12 @@ export default function RootLayout({
       <body className={`${inter.className} 
       bg-gray-50 text-gray-950 relative`}>
         <div className='bg-[#fbe2e3] absolute -z-10 top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'></div>
-        {children}
         <div className='bg-[#dbd7fb] absolute -z-10 top-[-1rem] right-[-35rem] 
         h-[31.25rem] w-[50rem] rounded-full blur-[10rem] 
         sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]'></div>
+
+        <Header></Header>
+        {children}
       </body>
     </html>
   )
--- lib/data.ts
+++ lib/data.ts
@@ -1,10 +1,7 @@
 import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
-import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+import placeholder from '@/public/placeholder.jpg';
 
 export const links = [
   {
@@ -82,35 +79,35 @@ export const projectsData = [
     description:
       "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
     tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
+    imageUrl: placeholder,
   },
   {
     title: "Project 2",
     description:
       "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
     tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
+    imageUrl: placeholder,
   },
   {
     title: "Project 3",
     description:
       "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
     tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
+    imageUrl: placeholder,
   },
   {
     title: "Project 2",
     description:
       "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
     tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
+    imageUrl: placeholder,
   },
   {
     title: "Project 3",
     description:
       "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
     tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
+    imageUrl: placeholder,
   },
 ] as const;
 
