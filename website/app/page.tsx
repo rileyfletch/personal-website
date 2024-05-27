005c89df285044f05e697a5e84f9d7d5562df097
riley <89424122+nrfletcher@users.noreply.github.com>
2024-05-26 21:34:32 -0400

    animation && components

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -2,21 +2,38 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-    <main className="bg-prussian">
+    <main className="font-serif area">
+
+      <div>
+        <ul className="circles">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+      </div>
+      
 
       <div className="sticky top-0 p-8 flex items-center">
           <ul className="list-none ml-auto">
-            <li className="mb-2"><a href="#" className="hover:bg-red-700 text-2xl">About</a></li>
-            <li className="mb-2"><a href="#" className="hover:bg-red-700 text-2xl">Experience</a></li>
-            <li className="mb-2"><a href="#" className="hover:bg-red-700 text-2xl">Projects</a></li>
-            <li className="mb-2"><a href="#" className="hover:bg-red-700 text-2xl">Contact</a></li>
-            <li className="mb-2"><Link href="/blog" className="hover:bg-red-700 text-2xl">Blog</Link></li>
+            <li className="mb-2"><a href="#" className="hover:text-engineering text-2xl">About</a></li>
+            <li className="mb-2"><a href="#" className="hover:text-engineering text-2xl">Experience</a></li>
+            <li className="mb-2"><a href="#" className="hover:text-engineering text-2xl">Projects</a></li>
+            <li className="mb-2"><a href="#" className="hover:text-engineering text-2xl">Contact</a></li>
+            <li className="mb-2"><a href="#" className="hover:text-engineering text-2xl">Resume</a></li>
+            <li className="mb-2"><Link href="/blog" className="hover:text-engineering text-2xl">Blog</Link></li>
           </ul>
       </div>
 
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col">
         <section id="about" className="">
-          <h2 className="text-3xl font-bold mb-4 text-center">About Me</h2>
+          <h1 className="text-7xl font-bold mb-4 ml-10">Riley Fletcher is an aspiring software engineer</h1>
           <p className="">
             this will be a bunch of stuff
             this will be a bunch of stuff
@@ -35,8 +52,17 @@ export default function Page() {
           </p>
         </section>
 
+        <section id="experience" className="py-16">
+          <h2 className="text-5xl font-bold mb-4 text-center">Experience</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            <p>
+              text
+            </p>
+          </div>
+        </section>
+
         <section id="projects" className="py-16">
-          <h2 className="text-3xl font-bold mb-4 text-center">Projects</h2>
+          <h2 className="text-5xl font-bold mb-4 text-center">Projects</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             <p>
               text
@@ -45,19 +71,17 @@ export default function Page() {
         </section>
 
         <section id="contact" className="py-16">
-          <h2 className="text-3xl font-bold mb-4 text-center">Contact</h2>
+          <h2 className="text-5xl font-bold mb-4 text-center">Contact</h2>
           <form className="max-w-md">
           </form>
         </section>
 
-        <footer className="bg-gray-800 text-white py-4">
+        <footer className="text-white py-4">
           <div className="container mx-auto text-center">
-            &copy; {new Date().getFullYear()} Your Name
+            &copy; {new Date().getFullYear()} Riley Fletcher
           </div>
         </footer>
       </div>
-
-      
 
     </main>
   );
---
+++ website/components/animation.tsx
@@ -0,0 +1,5 @@
+export default function animation() {
+    return (
+        <div></div>
+    )
+};
