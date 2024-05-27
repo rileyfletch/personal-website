402016f18298f4087d99731f6e6d2d7c09172996
riley <89424122+nrfletcher@users.noreply.github.com>
2024-05-26 21:52:47 -0400

    high level breakdown

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -18,16 +18,15 @@ export default function Page() {
             <li></li>
           </ul>
       </div>
-      
 
       <div className="sticky top-0 p-8 flex items-center">
           <ul className="list-none ml-auto">
-            <li className="mb-2"><a href="#" className="hover:text-engineering text-2xl">About</a></li>
-            <li className="mb-2"><a href="#" className="hover:text-engineering text-2xl">Experience</a></li>
-            <li className="mb-2"><a href="#" className="hover:text-engineering text-2xl">Projects</a></li>
-            <li className="mb-2"><a href="#" className="hover:text-engineering text-2xl">Contact</a></li>
-            <li className="mb-2"><a href="#" className="hover:text-engineering text-2xl">Resume</a></li>
-            <li className="mb-2"><Link href="/blog" className="hover:text-engineering text-2xl">Blog</Link></li>
+            <li className="mb-2"><a href="#" className="hover:text-engineering text-2xl link-underline link-underline-black">About</a></li>
+            <li className="mb-2"><a href="#" className="hover:text-engineering text-2xl link-underline link-underline-black">Experience</a></li>
+            <li className="mb-2"><a href="#" className="hover:text-engineering text-2xl link-underline link-underline-black">Projects</a></li>
+            <li className="mb-2"><a href="#" className="hover:text-engineering text-2xl link-underline link-underline-black">Contact</a></li>
+            <li className="mb-2"><a href="#" className="hover:text-engineering text-2xl link-underline link-underline-black">Resume</a></li>
+            <li className="mb-2"><Link href="/blog" className="hover:text-engineering text-2xl link-underline link-underline-black">Blog</Link></li>
           </ul>
       </div>
 
@@ -35,20 +34,16 @@ export default function Page() {
         <section id="about" className="">
           <h1 className="text-7xl font-bold mb-4 ml-10">Riley Fletcher is an aspiring software engineer</h1>
           <p className="">
-            this will be a bunch of stuff
-            this will be a bunch of stuff
+            paragraph about my achievements so far (high level)
           </p>
           <p className="">
-            this will be a bunch of stuff
-            this will be a bunch of stuff
+            paragraph about my future goals (high level)
           </p>
           <p className="">
-            this will be a bunch of stuff
-            this will be a bunch of stuff
+            paragraph about my technical interests
           </p>
           <p className="">
-            this will be a bunch of stuff
-            this will be a bunch of stuff
+            paragraph about what i do outside of computer stuff
           </p>
         </section>
 
@@ -56,7 +51,7 @@ export default function Page() {
           <h2 className="text-5xl font-bold mb-4 text-center">Experience</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             <p>
-              text
+              have a good looking section for each past work experience (provide an image and a good description of what i did)
             </p>
           </div>
         </section>
@@ -65,15 +60,16 @@ export default function Page() {
           <h2 className="text-5xl font-bold mb-4 text-center">Projects</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             <p>
-              text
+              have project cards here with a general description/technologies and is clickable to see github
             </p>
           </div>
         </section>
 
         <section id="contact" className="py-16">
           <h2 className="text-5xl font-bold mb-4 text-center">Contact</h2>
-          <form className="max-w-md">
-          </form>
+          <div>
+            <p>have clickable icons for all stuff (github/email/linkedin/devpost etc.)</p>
+          </div>
         </section>
 
         <footer className="text-white py-4">
