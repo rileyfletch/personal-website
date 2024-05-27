1b0eef3e2e39a895b7fae6b539547880b8483e76
riley <89424122+nrfletcher@users.noreply.github.com>
2024-05-27 18:19:43 -0400

    about section done

--- website/components/about.tsx
+++ website/components/about.tsx
@@ -2,29 +2,35 @@ export default function AboutSection() {
   return (
     <section id="about" className="py-16 flex flex-col items-center">
       <h1 className="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white pr-10 pb-1 text-5xl text-white font-bold text-center mb-10">
-        A student and aspiring software engineer
+        Aspiring to contribute to the world of compute
       </h1>
 
       <div className="max-w-5xl">
-        <p className="text-2xl mb-5">Hi, I&apos;m Riley 👋</p>
+        <p className="font-bold text-2xl mb-5">Hi, I&apos;m Riley 👋</p>
 
         <p className="text-2xl mb-5">
-          I&apos;m currently studying at the University of Virginia majoring in computer science, with a minor in data science. 
+          I&apos;m currently studying at the <a href="https://www.virginia.edu/" 
+          className="hover:text-engineering transition duration-500 underline underline-offset-4 decoration-green-500 hover:decoration-blue-700">University of Virginia
+          </a> majoring in <span className="italic">computer science</span>, with a minor in <span className="italic">data science</span>. 
           I&apos;m a teaching assistant for CSO1 and CSO2, the first two introductory courses in computer systems which are a part of the core UVA CS curriculum.
-          Additionally, I have been a part of theCourseForum as a developer, and a member of Motorsports Club and Outdoors Club.
+          Additionally, I have been a part of <a href="https://thecourseforum.com/" 
+          className="hover:text-engineering transition duration-500 underline underline-offset-4 decoration-green-500 hover:decoration-blue-700">theCourseForum</a> as a developer, a member of Virginia Motorsports, and Outdoors Club.
+          I greatly enjoy teaching others and collaborating on fun, interesting, or innovative projects.
         </p>
 
         <p className="text-2xl mb-5">
           I truly love all things tech, but I&apos;m particularly interested in systems. 
-          Anything to do with compilers, networks, operating systems, security, device drivers, and how these areas interact with hardware, all fascinate me. 
-          Consequently, I&apos;ve also become interested in learning about embedded systems, virtualization, and concurrent programming.
-          Aside from that, I enjoy learning about data science and machine learning, and really any sort of backend programming interests me! If it isn&apos;t obvious
-          from this website, design is not my strong suite.
+          Anything to do with networks, operating systems, compilers, cybersecurity, and how these areas interact with hardware, all fascinate me. 
+          Consequently, I&apos;ve also become interested in learning about embedded systems, device drivers, and virtualization.
+          Aside from that, I like to toy with data science/machine learning, and really any sort of backend programming interests me. If it isn&apos;t obvious
+          from this site, web design is not my strong suit.
         </p>
 
         <p className="text-2xl mb-5">
-          Aside from computer stuff, I love music (more so listening than playing these days, but I dabble in guitar), reading books, and trying to stay in shape either mountain biking, 
-          playing tennis/pickleball, or occasionally going to the gym. I also love to learn about history, finance, and fast cars. I also have a Letterboxd if you want 
+          Aside from computer stuff, I love music (more so listening than playing these days, but I dabble in guitar), reading books, 
+          and trying to stay in shape by either mountain biking, 
+          playing tennis/pickleball, or <span className="italic">occasionally</span> going to the gym. I also love to learn about history, finance, and fast cars. I also have a <a href="https://letterboxd.com/rileyfletcher/" 
+          className="hover:text-engineering transition duration-500 underline underline-offset-4 decoration-green-500 hover:decoration-blue-700">Letterboxd</a> if you want 
           to judge my horrific movie ratings.
         </p>
       </div>
--- website/components/projects.tsx
+++ website/components/projects.tsx
@@ -1,11 +1,11 @@
+import ProjectCard from "./project-card";
+
 export default function ProjectSection() {
     return (
         <section id="projects" className="py-16">
           <h2 className="text-5xl font-bold mb-4 text-center transition-colors duration-700 hover:text-engineering">Projects</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 items-center">
-            <p>
-              have project cards here with a general description/technologies and is clickable to see github
-            </p>
+            <ProjectCard></ProjectCard>
           </div>
         </section>
     )
