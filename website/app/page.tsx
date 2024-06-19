7042f00bfae273d08624d0bb7d9853ac530d521f
riley <89424122+nrfletcher@users.noreply.github.com>
2024-06-18 22:55:13 -0400

    deleted

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -1,27 +1,19 @@
 import AboutSection from "@/components/about";
-import ContactSection from "@/components/contact";
-import ExperienceSection from "@/components/experience";
-import Footer from "@/components/footer";
 import IntroSection from "@/components/intro";
-import NavigationSidebar from "@/components/navbar";
-import ProjectSection from "@/components/projects";
 
 export default function Page() {
   return (
-    <main className="font-serif flex flex-row text-prussian">
+    <main className="font-serif flex flex-row text-medium bg-bg">
 
+    
       <div className="flex flex-col items-center">
         <IntroSection></IntroSection>
-        <ExperienceSection></ExperienceSection>
-        <ProjectSection></ProjectSection>
+        
         <AboutSection></AboutSection>
-        <ContactSection></ContactSection>
-        <Footer></Footer>
+        
       </div>
 
-      <div className="sticky top-0 right-0">
-        <NavigationSidebar />
-      </div>
+      
 
     </main>
   );
--- website/components/footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className="text-white py-4">
-          <div className="container mx-auto text-center transition-colors duration-700 hover:text-persian">
-            &copy; {new Date().getFullYear()} Riley Fletcher
-          </div>
-        </footer>
-    )
-}
