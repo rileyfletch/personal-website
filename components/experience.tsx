df5c54c7932827c3afc9ad929b7e1c139acabbf2
riley <89424122+nrfletcher@users.noreply.github.com>
2024-06-20 23:41:42 -0400

    hero section done

--- components/experience.tsx
+++ components/experience.tsx
@@ -2,18 +2,18 @@ import Image from "next/image";
 
 export default function Experience() {
     return (
-        <div className="text-center p-8">
+        <div className="text-center md:px-48 space-y-24">
             <h2 className="font-semibold text-2xl sm:text-3xl md:text-4xl lg:text-5xl">
                 Experience
             </h2>
 
             <div className="flex flex-wrap items-center mt-20 text-left text-center">
                 <div className="w-full md:w-3/5 lg:w-1/2 px-4">
-                    <Image src="train.svg" alt="gem" className="inline-block rounded shadow-lg border border-merino-400" width={200} height={200}></Image>
+                    <Image src="dell.svg" alt="gem" className="inline-block" width={200} height={200}></Image>
                 </div>
                 <div className="w-full md:w-2/5 lg:w-1/2 px-4 text-center md:text-left lg:pl-12">
                     <h3 className="font-bold mt-8 text-xl md:mt-0 sm:text-2xl">
-                        Dell, Software Engineering Intern
+                        Dell Technologies, <span className="font-medium">Software Engineering Intern</span>
                     </h3>
                     <h2 className="font-light mt-10 text-xl md:mt-2 sm:text-1xl">
                         May 2024 - August 2024
@@ -28,11 +28,11 @@ export default function Experience() {
 
             <div className="flex flex-wrap items-center mt-20 text-left text-center">
                 <div className="w-full md:w-3/5 lg:w-1/2 px-4">
-                    <Image src="train.svg" alt="project members" className="inline-block rounded shadow-lg border border-merino-400" width={200} height={200}></Image>
+                    <Image src="navy.svg" alt="project members" className="inline-block" width={200} height={200}></Image>
                 </div>
                 <div className="w-full md:w-2/5 lg:w-1/2 px-4 md:order-first text-center md:text-left lg:pr-12">
                 <h3 className="font-bold mt-8 text-xl md:mt-0 sm:text-2xl">
-                        Dell, Software Engineering Intern
+                    United States Navy, <span className="font-medium">Software Engineering Intern</span>
                     </h3>
                     <h2 className="font-light mt-10 text-xl md:mt-2 sm:text-1xl">
                         May 2024 - August 2024
@@ -47,11 +47,11 @@ export default function Experience() {
 
             <div className="flex flex-wrap items-center mt-20 text-left  text-center">
                 <div className="w-full md:w-3/5 lg:w-1/2 px-4">
-                    <Image src="train.svg" alt="editor" className="inline-block rounded shadow-lg border border-merino-400" width={200} height={200}></Image>
+                    <Image src="/uvalogo.png" alt="editor" className="inline-block" width={250} height={250}></Image>
                 </div>
                 <div className="w-full md:w-2/5 lg:w-1/2 px-4 text-center md:text-left lg:pl-12">
                     <h3 className="font-bold mt-8 text-xl md:mt-0 sm:text-2xl">
-                            Dell, Software Engineering Intern
+                        University of Virginia, <span className="font-medium">Teaching Assistant</span>
                         </h3>
                         <h2 className="font-light mt-10 text-xl md:mt-2 sm:text-1xl">
                             May 2024 - August 2024
@@ -70,7 +70,7 @@ export default function Experience() {
                 </div>
                 <div className="w-full md:w-2/5 lg:w-1/2 px-4 md:order-first text-center md:text-left lg:pr-12">
                     <h3 className="font-bold mt-8 text-xl md:mt-0 sm:text-2xl">
-                            Dell, Software Engineering Intern
+                        Virginia Railway Express, <span className="font-medium">Technology Intern</span>
                         </h3>
                         <h2 className="font-light mt-10 text-xl md:mt-2 sm:text-1xl">
                             May 2024 - August 2024
--- components/hero.tsx
+++ components/hero.tsx
@@ -3,14 +3,14 @@ import Image from "next/image";
 export default function Hero() {
     return (
         <section className="min-h-screen flex items-center pb-16">
-            <div className="container mx-auto md:px-24 md:py-10">
+            <div className="container mx-auto md:px-48 md:py-10">
                 <div className="flex flex-col md:flex-row items-center">
                     <div className="w-full md:w-1/2 mb-8 md:mb-0 md:pr-8 text-center md:text-left">
                         <h1 className="text-3xl font-extrabold leading-tight mb-4 sm:text-4xl md:text-5xl">
                         Hi, I&apos;m Riley
                         </h1>
                         <p className="font-medium mb-8 max-w-2xl mx-auto md:mx-0 leading-relaxed dark:text-gray-300 sm:text-lg">
-                        I&apos;m an aspiring software engineer with a passion for backend development, an interest in systems, and a hunger to solve unique problems
+                        I&apos;m an aspiring software engineer with a passion for backend development, computer systems, and solving unique problems.
                         </p>
                         <div className="flex flex-wrap justify-center md:justify-start gap-4">
                         <a href="#" className="inline-flex text-white bg-pastel6 border-0 py-2 px-6 focus:outline-none hover:bg-pastel5 rounded-xl text-lg transition-colors duration-300 ease-in-out">
