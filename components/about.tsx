119aed7c748dbbd3249853a0d4afaf9771212ad9
riley <89424122+nrfletcher@users.noreply.github.com>
2024-06-23 22:13:54 -0400

    about done prelim

--- components/about.tsx
+++ components/about.tsx
@@ -1,24 +1,55 @@
+import Image from "next/image"
+
 export default function About() {
     return (
-        <div className="flex flex-col items-center pt-8">
-            <h2 className="">About Me</h2>
-            <p className="sm:text-lg mt-3">Hey, I&apos;m Riley, and I&apos;m currently studying computer and data science at the University of Virginia.
+        <div className="flex flex-col items-center mb-10">
 
-            </p>
-
-            <div className="mt-20">
-                    <h3 className="font-bold mt-8 text-xl md:mt-0 sm:text-2xl">
-                        Dell Technologies, <span className="font-medium">Software Engineering Intern</span>
-                    </h3>
-                    <h2 className="font-light mt-10 text-xl md:mt-2 sm:text-1xl">
-                        May 2024 - August 2024
+            <div className="flex flex-row items-center mb-6 mt-4">
+                    <h2 className="pr-2 text-xl font-medium leading-tight mb-1 sm:text-4xl md:text-5xl text-center transition-colors duration-1000 ease-in-out hover:text-red">
+                            About
                     </h2>
-                    <p className=" mt-3">
-                        Creating Python utilities for NVMe drive performance monitoring and analysis for the PowerStore backend protocols team.
-                    </p>
+                    <Image src="codesquare.svg" alt="code" width={50} height={50}></Image>
             </div>
 
-            <div className="mt-20">
+            <div className="max-w-5xl mx-auto space-y-5">
+                <p className="sm:text-lg mb-4">
+                    Hi, I&apos;m Riley, and I&apos;m currently studying computer and data science at 
+                    the <a href="https://www.virginia.edu/" className="underline hover:text-blue transition-colors duration-700 ease-in-out">University of Virginia</a>.
+                    Besides taking classes, I&apos;m a teaching assistant for Computer Systems and Organization, 
+                    and a student developer at <a href="https://thecourseforum.com" className="underline hover:text-red transition-colors duration-700 ease-in-out">theCourseForum</a>. 
+                    Cliche as it sounds, I love all things technology and computers. 
+                    However, I find particular interest in areas such as operating systems,
+                    embedded systems, networks, security, and machine learning.
+                </p>
+
+                <p className="sm:text-lg">
+                    Outside of computers, I love to listen to music, read, and play a variety of sports. 
+                    Lately, I&apos;ve been having fun mountain biking.
+                    As much as I enjoy living in an IDE for hours, getting outside is crucial to me, and I love to explore new places. 
+                </p>
+
+            </div>
+
+            <div className="flex flex-row items-center mb-6 mt-6">
+                <h2 className="pr-2 text-medium font-medium leading-tight mb-2 sm:text-4xl md:text-5xl text-center transition-colors duration-1000 ease-in-out hover:text-red">
+                        Experience
+                </h2>
+            </div>
+
+            <div className="max-w-6xl mx-auto space-y-10">
+            <div>
+                <h3 className="font-bold text-xl sm:text-2xl">
+                        Dell Technologies, <span className="font-medium">Software Engineering Intern</span>
+                    </h3>
+                    <h2 className="font-light text-xl mt-2">
+                        May 2024 - August 2024
+                    </h2>
+                    <p className="mt-3 sm:text-lg">
+                        Creating Python utilities for NVMe drive performance monitoring and analysis for the PowerStore backend protocols team.
+                    </p>
+                </div>
+
+            <div>
                 <h3 className="font-bold mt-8 text-xl md:mt-0 sm:text-2xl">
                     United States Navy, <span className="font-medium">Software Engineering Intern</span>
                     </h3>
@@ -32,20 +63,20 @@ export default function About() {
                     </p>
             </div>
 
-            <div className="mt-20">
-                    <h3 className="font-bold mt-8 text-xl md:mt-0 sm:text-2xl">
-                        University of Virginia, <span className="font-medium">Teaching Assistant</span>
-                        </h3>
-                        <h2 className="font-light mt-10 text-xl md:mt-2 sm:text-1xl">
-                            January 2024 - Current
-                        </h2>
-                        <p className="sm:text-lg mt-3">
-                        Supported Computer Systems and Organization I & II courses. Hosted labs, office hours, and exam prep sessions. 
-                        Topics included hardware design, circuits, x86 assembly, and C programming for systems concepts like concurrency, virtual memory, and Linux internals.
-                    </p>
+            <div>
+                <h3 className="font-bold mt-8 text-xl md:mt-0 sm:text-2xl">
+                    University of Virginia, <span className="font-medium">Teaching Assistant</span>
+                    </h3>
+                    <h2 className="font-light mt-10 text-xl md:mt-2 sm:text-1xl">
+                        January 2024 - Current
+                    </h2>
+                    <p className="sm:text-lg mt-3">
+                    Supported Computer Systems and Organization I & II courses. Hosted labs, office hours, and exam prep sessions. 
+                    Topics included hardware design, circuits, x86 assembly, and C programming for systems concepts like concurrency, virtual memory, and Linux internals.
+                </p>
             </div>
 
-            <div className="mt-20">
+            <div>
                 <h3 className="font-bold mt-8 text-xl md:mt-0 sm:text-2xl">
                 Virginia Railway Express, <span className="font-medium">Technology Intern</span>
                 </h3>
@@ -56,6 +87,7 @@ export default function About() {
                 Automated data analysis and workstation setup. Offered IT support for cloud and collaboration tools. 
                 Maintained network infrastructure across office and remote sites, including train stations.
                 </p>
+            </div>
             </div>
 
         </div>
--- components/projects.tsx
+++ components/projects.tsx
@@ -2,74 +2,89 @@ import Image from "next/image";
 
 export default function Projects() {
     return (
-    <div className="px-2 py-8 bg-cream">
-        <div id="features" className="mx-auto max-w-6xl">
-            <h2 className="text-xl font-smibold leading-tight mb-4 sm:text-4xl md:text-5xl text-center">
-                    Projects
-            </h2>
+    <div className="px-2 py-8 bg-cream flex flex-col items-center">
+        <div className="flex flex-row items-center">
+                <h2 className="pr-2 text-xl font-medium leading-tight mb-1 sm:text-4xl md:text-5xl text-center transition-colors duration-1000 ease-in-out hover:text-red">
+                        Projects
+                </h2>
+                <Image src="codesquare.svg" alt="code" width={50} height={50}></Image>
+        </div>
 
-            <ul className="mt-8 grid grid-cols-1 gap-6 text-center text-slate-700 md:grid-cols-3">
-            <li className="rounded-3xl px-6 py-8 shadow-sm transition-colors duration-1000 ease-in-out hover:bg-lblue">
+        <p className="text-center font-medium text-lg mt-8">Building projects is a fun way to learn new languages and technologies.</p>
+        
+        <div id="features" className="mx-auto max-w-6xl mt-8">
+            <ul className="mt-4 grid grid-cols-1 gap-6 text-center text-slate-700 md:grid-cols-3">
 
+            <li className="rounded-3xl px-6 py-8 shadow-sm transition-colors duration-1000 ease-in-out hover:bg-blue">
                  <a href="/pricing" className="group">
-                <Image src="https://www.svgrepo.com/show/530440/machine-vision.svg" alt="" className="mx-auto h-10 w-10" width={200} height={200}></Image>
+                <Image src="processor.svg" alt="" className="mx-auto h-10 w-10" width={200} height={200}></Image>
                 <h3 className="my-3 font-display font-medium group-hover:text-primary-500">Out-of-order Processor</h3>
                 <p className="mt-1.5 text-sm leading-6 text-secondary-500">Simple processor emulator with program loader, debugger, and 
                     tooling to understand aspects of out-of-order program execution.
                 </p>
                 </a>
-
             </li>
-            <li className="rounded-3xl px-6 py-8 shadow-sm transition-colors duration-1000 ease-in-out hover:bg-lblue">
 
+            <li className="rounded-3xl px-6 py-8 shadow-sm transition-colors duration-1000 ease-in-out hover:bg-blue">
                 <a href="/pricing" className="group">
-                <Image src="https://www.svgrepo.com/show/530440/machine-vision.svg" alt="" className="mx-auto h-10 w-10" width={200} height={200}></Image>
+                <Image src="map.svg" alt="" className="mx-auto h-10 w-10" width={200} height={200}></Image>
                 <h3 className="my-3 font-display font-medium group-hover:text-primary-500">HistoryMaps</h3>
                 <p className="mt-1.5 text-sm leading-6 text-secondary-500">An interactive web application that allows users to explore how
                     geography and history mesh together. 
                 </p>
                 </a>
-
             </li>
-            <li className="rounded-3xl px-6 py-8 shadow-sm transition-colors duration-1000 ease-in-out hover:bg-lblue">
 
+            <li className="rounded-3xl px-6 py-8 shadow-sm transition-colors duration-1000 ease-in-out hover:bg-blue">
                 <a href="/pricing" className="group">
-                <Image src="https://www.svgrepo.com/show/530440/machine-vision.svg" alt="" className="mx-auto h-10 w-10" width={200} height={200}></Image>
+                <Image src="question.svg" alt="" className="mx-auto h-10 w-10" width={200} height={200}></Image>
                 <h3 className="my-3 font-display font-medium group-hover:text-primary-500">TriviaBot</h3>
                 <p className="mt-1.5 text-sm leading-6 text-secondary-500">Discord bot that adds trivia to a channel, and an accompanying
                     server analytics application using PyQt and MySQL.
                 </p>
                 </a>
-
             </li>
-            <li className="rounded-3xl px-6 py-8 shadow-sm transition-colors duration-1000 ease-in-out hover:bg-lblue">
+
+            <li className="rounded-3xl px-6 py-8 shadow-sm transition-colors duration-1000 ease-in-out hover:bg-blue">
                 <a href="/pricing" className="group">
-                <Image src="https://www.svgrepo.com/show/530440/machine-vision.svg" alt="" className="mx-auto h-10 w-10" width={200} height={200}></Image>
+                <Image src="chess.svg" alt="" className="mx-auto h-10 w-10" width={200} height={200}></Image>
                 <h3 className="my-3 font-display font-medium group-hover:text-primary-500">Swing Chess</h3>
                 <p className="mt-1.5 text-sm leading-6 text-secondary-500">A simple two-player chess game built using Java Swing that implements
                     the Model-View-Controller pattern.
                 </p>
                 </a>
             </li>
-            <li className="rounded-3xl px-6 py-8 shadow-sm transition-colors duration-1000 ease-in-out hover:bg-lblue">
+
+            <li className="rounded-3xl mb-8 px-6 py-8 shadow-sm transition-colors duration-1000 ease-in-out hover:bg-blue">
+                <a href="/download" className="group">
+                <Image src="bot.svg" alt="" className="mx-auto h-10 w-10" width={200} height={200}></Image>
+                <h3 className="my-3 font-display font-medium group-hover:text1">Personalized GPT Chatbot</h3>
+                <p className="mt-1.5 text-sm leading-6 text-secondary-500">Add a Discord bot to your server which can utilize an internal corpus of knowledge to answer questions.
+                </p>
+                </a>
+            </li>
+
+            <li className="rounded-3xl mb-8 px-6 py-8 shadow-sm transition-colors duration-1000 ease-in-out hover:bg-blue">
+                <a href="/download" className="group">
+                <Image src="website.svg" alt="" className="mx-auto h-10 w-10" width={200} height={200}></Image>
+                <h3 className="my-3 font-display font-medium group-hover:text1">Personal Website</h3>
+                <p className="mt-1.5 text-sm leading-6 text-secondary-500">The second iteration of my personal site. My web stack consists of Next.js with TypeScript, TailwindCSS, and Framer Motion.
+                </p>
+                </a>
+            </li>
+
+            <li className="rounded-3xl px-6 py-8 shadow-sm transition-colors duration-1000 ease-in-out hover:bg-blue">
                 <a href="/templates" className="group">
-                <Image src="https://www.svgrepo.com/show/530450/page-analysis.svg" alt="" className="mx-auto h-10 w-10" width={200} height={200}></Image>
+                <Image src="messages.svg" alt="" className="mx-auto h-10 w-10" width={200} height={200}></Image>
                 <h3 className="my-3 font-display font-medium group-hover:text-primary-500">
                     SocketTalk
                 </h3>
                 <p className="mt-1.5 text-sm leading-6 text-secondary-500">JavaFX application for talking to other computers on a local network.</p>
                 </a>
             </li>
-            <li className="rounded-3xl mb-8 px-6 py-8 shadow-sm transition-colors duration-1000 ease-in-out hover:bg-lblue">
-                <a href="/download" className="group">
-                <Image src="https://www.svgrepo.com/show/530453/mail-reception.svg" alt="" className="mx-auto h-10 w-10" width={200} height={200}></Image>
-                <h3 className="my-3 font-display font-medium group-hover:text1">Personalized GPT Chatbot</h3>
-                <p className="mt-1.5 text-sm leading-6 text-secondary-500">Add a Discord bot to your server which can utilize an internal corpus of knowledge to answer questions.
-                </p>
-                </a>
-            </li>
-            </ul>
 
+
+            </ul>
         </div>
     </div>
     )
