474f25a1d18cebe7f0f458ea56ea0541c3f4c283
riley <89424122+nrfletcher@users.noreply.github.com>
2024-06-30 19:07:39 -0400

    about done

--- components/about.tsx
+++ components/about.tsx
@@ -8,12 +8,12 @@ export default function About() {
 
                 <p className="sm:text-xl mb-4">
                     Hi, I&apos;m Riley, and I&apos;m currently studying computer science and data science at the 
-                     <a className="text-red font-semibold" href="https://engineering.virginia.edu/department/computer-science"> University of Virginia </a>
+                     <a className="text-red font-semibold transition-all duration-700 hover:text-blue" href="https://engineering.virginia.edu/department/computer-science"> University of Virginia </a>
                     in Charlottesville, graduating in the Spring of 2025. 
                     </p> 
                     <p className="sm:text-xl mb-4">
                     At UVA, I&apos;m a teaching assistant for Computer Systems and Organization I and II,
-                    and a developer at <a className="text-red font-semibold" href="https://thecourseforum.com/"> theCourseForum </a>. I&apos;ve been a software engineering intern at Dell Technologies, the United
+                    and a developer at <a className="text-red font-semibold transition-all duration-700 hover:text-green" href="https://thecourseforum.com/"> theCourseForum </a>. I&apos;ve been a software engineering intern at Dell Technologies, the United
                     States Navy, and worked as an information technology intern at Virginia Railway Express.
                 </p>
 
@@ -22,23 +22,22 @@ export default function About() {
                     networks, security, embedded systems, and backend web development. </p>
                     <p className="sm:text-xl mb-4">
                     The complexity of computer
-                    systems fascinates me. I&apos;m always trying to build knowledge of the stack, from transistors
+                    systems fascinates me. I&apos;m always trying to increase knowledge of the stack, from transistors
                     all the way to - how does JavaScript run in a browser?
                 </p>
 
                 <p className="sm:text-xl">
                     When I&apos;m not indulging in tech, I love to read and write, listen to music, attempt to play guitar,
-                    ride my mountainbike, and get outside exploring nature. If you&apos;re curious about my work experience
+                    mountain bike, and get outside exploring nature. If you&apos;re curious about my work experience
                     in further detail, continue reading.
                 </p>
 
                 <p className="sm:text-xl mb-4 font-semibold">My experience in technology.</p>
 
                 <p className="sm:text-xl"> 
-                    Currently, I am a software engineering intern at Dell Technologies working for the Powerstore team
-                    on the backend protocols team. I&apos;m currently working on Python utilities for IO workload automation
-                    and reporting, as well as IO tracing and analysis. This role has been super fun so far as I am very
-                    interested in gaining more opportunities to work with systems software and hardware appliances.
+                    During the Summer of 2024, I am a software engineering intern at <a className="text-red font-semibold transition-all duration-700 hover:text-blue" href="https://www.dell.com/en-us/shop/storage-servers-and-networking-for-business/sf/power-store"> Dell Technologies </a> working on Powerstore
+                    on the backend protocols team. I&apos;m currently working on developing utilies for IO job testing and automation,
+                    as well as implementing IO trace features to identify bottlenecks and increase developer debugging efficiency.
                 </p>
 
                 <p className="sm:text-xl">
@@ -52,7 +51,7 @@ export default function About() {
 
                 <p className="sm:text-xl">
                     The Summer of 2023, after my final year at community college, I was a software engineering intern
-                    for the United States Navy, at the Dahlgren research facility. As a part of the CIAT team,
+                    for the United States Navy, at the <a className="text-red font-semibold transition-all duration-700 hover:text-blue" href="https://www.navsea.navy.mil/Media/News/Article/1929358/navy-warfare-center-engineers-revolutionize-aegis-combat-system-training-with-c/"> Dahlgren research facility </a>. As a part of the CIAT team,
                     I worked on naval training software where I spent most of my time improving the existing software by
                     addressing security concerns, outdated programming methodologies, and adding unit tests. I was able
                     to experience working on a large software project, and was introduced to things like CI/CD, agile
@@ -60,8 +59,8 @@ export default function About() {
                 </p>
 
                 <p className="sm:text-xl">
-                From the Summer of 2022 to Spring of 2023, I was an information technology intern at Virginia
-                Railway Express. As my first &apos;proper&apos; technical role,
+                From the Summer of 2022 to Spring of 2023, I was an information technology intern at <a className="text-red font-semibold transition-all duration-700 hover:text-blue" href="https://www.vre.org/"> Virginia Railway Express </a>
+                . As my first &apos;proper&apos; technical role,
                 I learned a lot about being in a professional environment. From a technical perspective, I did
                 a lot of physical networking both in-office and at train stations, shell scripting to automate
                 tasks and aggegrate data, and general help desk stuff. I ended up working here part time until the Summer
@@ -71,12 +70,16 @@ export default function About() {
                 <p className="sm:text-xl mb-4 font-semibold">Me personally, continued.</p>
 
                 <p className="sm:text-xl">
-                I am a bit addicted to keeping track of things I do, so it is quite easy for you to get a glimpse into
-                my taste of things. I try to log everything I read on Goodreads, all my movie history on Letterboxd,
-                and my album listens on RateYourMusic. I welcome all judgement toward my questionable takes.
+                I am a bit addicted to keeping track of what media I consume; I try to log everything I read on 
+                <a className="text-red font-semibold transition-all duration-700 hover:text-blue" 
+                    href="https://www.goodreads.com/user/show/133370871-riley-fletcher"> Goodreads
+                </a>, all my movie history on <a className="text-red font-semibold transition-all duration-700 hover:text-blue" 
+                    href="https://letterboxd.com/rileyfletcher/">
+                 Letterboxd</a>,
+                and my album listens on <a className="text-red font-semibold transition-all duration-700 hover:text-blue" 
+                    href="https://rateyourmusic.com/~rileyfletch">
+                RateYourMusic</a>. Feel free to peruse my questionable takes.
                 </p>
-
-
             </div>
         </div>
     )
