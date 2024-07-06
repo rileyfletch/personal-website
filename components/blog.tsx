74fe15c3346bec1e896c9e663a1d0779c5848a8f
riley <89424122+nrfletcher@users.noreply.github.com>
2024-07-06 18:55:31 -0400

    Link to fix basepath issue

--- components/blog.tsx
+++ components/blog.tsx
@@ -1,10 +1,12 @@
+import Link from "next/link";
+
 export default function Blog() {
     return (
         <div className="mt-10">
             <ul className="text-center text-xl">
                 <li>
-                    <a href="/hello-world" 
-                        className="text-dblue font-bold mr-4 transition-all duration-700 hover:text-red">A first post.</a>July 3rd, 2024
+                    <Link href="/hello-world" 
+                        className="text-dblue font-bold mr-4 transition-all duration-700 hover:text-red">A first post.</Link>July 3rd, 2024
                 </li>
             </ul>
         </div>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,19 +1,19 @@
-import Image from "next/image";
+import Link from "next/link"
 
 export default function Navigation() {
     return(
         <nav className="py-3 px-4 flex items-center justify-between">
             <div className="flex flex-row">
-                <a className="font-medium text-2xl tracking-tight transition-all duration-700 hover:text-red pr-2" href="/">
+                <Link className="font-medium text-2xl tracking-tight transition-all duration-700 hover:text-red pr-2" href="/">
                 riley fletcher 
-                </a>
+                </Link>
             </div>
             
             <div className="flex items-center">
-                <a className="font-medium text-md px-4 py-2 leading-none rounded-full tracking-tight transition-all duration-700 hover:text-blue" href="/about">about</a>
-                <a className="font-medium text-md px-4 py-2 leading-none rounded-full tracking-tight transition-all duration-700 hover:text-green" href="/projects">projects</a>
-                <a className="font-medium text-md px-4 py-2 leading-none rounded-full tracking-tight transition-all duration-700 hover:text-blue" href="/blog">blog</a>
-                <a className="font-medium text-md px-4 py-2 leading-none rounded-full tracking-tight transition-all duration-700 hover:text-green" href="resume.pdf" download="fletcher_resume">resume</a>
+                <Link className="font-medium text-md px-4 py-2 leading-none rounded-full tracking-tight transition-all duration-700 hover:text-blue" href="/about">about</Link>
+                <Link className="font-medium text-md px-4 py-2 leading-none rounded-full tracking-tight transition-all duration-700 hover:text-green" href="/projects">projects</Link>
+                <Link className="font-medium text-md px-4 py-2 leading-none rounded-full tracking-tight transition-all duration-700 hover:text-blue" href="/blog">blog</Link>
+                <Link className="font-medium text-md px-4 py-2 leading-none rounded-full tracking-tight transition-all duration-700 hover:text-green" href="resume.pdf" download="fletcher_resume">resume</Link>
             </div>
         </nav>
     )
