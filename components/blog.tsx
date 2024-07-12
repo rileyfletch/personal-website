963746379c9bc45e0de4607ce05f67fc96df7f80
riley <89424122+nrfletcher@users.noreply.github.com>
2024-07-11 22:51:29 -0400

    blog spacing and line fixes

--- components/blog.tsx
+++ components/blog.tsx
@@ -2,12 +2,14 @@ import Link from "next/link";
 
 export default function Blog() {
     return (
-        <div className="mt-10">
-            <ul className="text-center text-xl">
+        <div className="flex justify-center mt-10">
+            <ul className="text-xl space-y-4">
                 <li>
                     <Link href="/making-a-site" 
                             className="text-dblue font-bold mr-4 transition-all duration-700 hover:text-red">The process of making this website.</Link>July 6th, 2024
-                    <Link href="/hello-world" 
+                </li>
+                <li>
+                <Link href="/hello-world" 
                         className="text-dblue font-bold mr-4 transition-all duration-700 hover:text-red">A first post.</Link>July 3rd, 2024
                 </li>
             </ul>
